35bb095725780635f9d423c579ed97352780d0c8
Jackie Ha <jha2@wellesley.edu>
2018-08-19 00:22:16 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,6 +14,8 @@ def speak_to_grandma(speak)
   if speak == speak.upcase
     "NO, NOT SINCE 1938!"
   elsif speak == "I LOVE YOU GRANDMA!"
-    
+    "I LOVE YOU TOO PUMPKIN!"
   else 
-    "HUH?! SPEAK UP, SONNY!
+    "HUH?! SPEAK UP, SONNY!"
+  end
+end
