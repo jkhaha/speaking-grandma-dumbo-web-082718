20e54fd71d636cf8385eaefa17463a5b58831cea
Jackie Ha <jha2@wellesley.edu>
2018-08-19 00:25:03 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,20 +2,21 @@
 def speak_to_grandma(phrase)
  if phrase == "I LOVE YOU GRANDMA!"
    return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == phrase.downcase
+  elsif phrase == phrase.upcase
+   return "NO, NOT SINCE 1938!"
+  else phrase == phrase.downcase
    return "HUH?! SPEAK UP, SONNY!"
-  #else phrase == phrase.upcase
-  #  return "NO, NOT SINCE 1938!"
-#end 
+ end 
+end 
 
 # speak_to_grandma("HI GRANDMA")
 
-def speak_to_grandma(speak)
-  if speak == speak.upcase
-    "NO, NOT SINCE 1938!"
-  elsif speak == "I LOVE YOU GRANDMA!"
-    "I LOVE YOU TOO PUMPKIN!"
-  else 
-    "HUH?! SPEAK UP, SONNY!"
-  end
-end
+#def speak_to_grandma(speak)
+#  if speak == speak.upcase
+#    "NO, NOT SINCE 1938!"
+#  elsif speak == "I LOVE YOU GRANDMA!"
+ #   "I LOVE YOU TOO PUMPKIN!"
+ # else 
+ #   "HUH?! SPEAK UP, SONNY!"
+ # end
+#end
